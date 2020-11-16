4f7efdd67876409a415774ba3900cd0c1884811b
Shane Wallace <colinwallace924@gmail.com>
2020-11-16 13:24:37 -0600

    Add files via upload

---
+++ lights.py
@@ -0,0 +1,32 @@
+import pigpio
+import time
+
+pi = pigpio.pi()
+
+RED_PIN = 17
+GREEN_PIN = 24
+BLUE_PIN = 22
+
+def set_color(red, green, blue, seconds=0.1):
+    pi.set_PWM_dutycycle(RED_PIN, red)
+    pi.set_PWM_dutycycle(GREEN_PIN, green)
+    pi.set_PWM_dutycycle(BLUE_PIN, blue)
+    print("RGB:", red, green, blue)
+    time.sleep(seconds)
+    
+set_color(0, 0, 0)
+
+input("Lights initialized. Press enter to start.")
+
+while True:
+    set_color(255, 0, 0)
+    set_color(255, 165, 0)
+    set_color(0, 255, 0)
+    set_color(0, 255, 165)
+    set_color(0, 0, 255)
+    set_color(75, 0, 130)
+    set_color(128, 0, 128)
+    set_color(0, 0, 0)
+    
+    
+pi.stop()
